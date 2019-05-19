e9ca15a58afa1b45b6e4067b8bbc1112cc81c126
lilytran997 <lytran997@gmail.com>
2019-05-19 20:13:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/assigmentweek6/UsersActivity.kt
@@ -0,0 +1,12 @@
+package com.example.assigmentweek6
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UsersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}
